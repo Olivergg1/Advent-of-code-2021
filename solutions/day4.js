4f274ff786d06924394b3fb4371fda1967d9e2ed
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2021-12-06 01:42:23 +0100

    hittade en lösning till dag4:del1

--- solutions/day4.js
+++ solutions/day4.js
@@ -1,10 +1,81 @@
 const fs = require('fs')
+const ROWS = 5
+const COLUMNS = 5
+
+const manageData = (data) => {
+  const newData = data.split('\r\n\r\n')
+  const numbersDrawn = newData[0].split(',').map((i) => Number(i))
+  const tiles = newData
+    .splice(1, newData.length)
+    .map((x) => x.split('\r\n'))
+    .map((tile) =>
+      tile.map((row) =>
+        row
+          .split(' ')
+          .filter((s) => s !== '')
+          .map((s) => Number(s))
+      )
+    )
+
+  return [numbersDrawn, tiles]
+}
+
+function intersect(a, b) {
+  var setB = new Set(b)
+  return a.filter((x) => setB.has(x))
+}
 
 const part1 = (data) => {
-  const newData = data.split('\r\n\r\n')
-  const numbersDrawn = newData[0].split(',')
-  const tiles = newData.splice(1, newData.length).map((x) => x.split('\r\n'))
-  console.log(tiles)
+  const [numbers, tiles] = manageData(data)
+  let drawnNumbers = []
+  let winningTile = null
+
+  for (let n of numbers) {
+    if (winningTile) break
+    drawnNumbers.push(n)
+    for (let t in tiles) {
+      for (let c = 0; c < COLUMNS; c++) {
+        if (winningTile) break
+        let column = []
+        for (let r = 0; r < ROWS; r++) {
+          if (winningTile) break
+          let matchingNumbers = intersect(tiles[t][r], drawnNumbers).sort()
+          let currentRow = [...tiles[t][r]].sort()
+          if (
+            matchingNumbers.length === ROWS &&
+            matchingNumbers.every((v, i) => v === currentRow[i])
+          ) {
+            winningTile = tiles[t]
+              .join(',')
+              .split(',')
+              .map((i) => Number(i))
+            break
+          }
+          column.push(tiles[t][r][c])
+        }
+
+        let matchingNumbers = ([] = intersect(column, drawnNumbers).sort())
+        let sortedColumn = column.sort()
+        if (
+          matchingNumbers.length === COLUMNS &&
+          matchingNumbers.every((v, i) => v === sortedColumn[i])
+        ) {
+          winningTile = tiles[t]
+            .join(',')
+            .split(',')
+            .map((i) => Number(i))
+          break
+        }
+        column = []
+      }
+    }
+  }
+
+  const markedNumbers = new Set(drawnNumbers)
+  const sumOfUnmarked = [...new Set(winningTile)]
+    .filter((x) => !markedNumbers.has(x))
+    .reduce((prev, curr) => prev + curr)
+  console.log(sumOfUnmarked * drawnNumbers[drawnNumbers.length - 1])
 }
 
 const main = () => {
