318f5f7c6d115cd40f35da64d8c8fd856ae991ac
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2021-12-07 19:53:22 +0100

    solved day5 #1

--- solutions/day5.js
+++ solutions/day5.js
@@ -1,37 +1,44 @@
-const { create } = require('domain')
 const fs = require('fs')
 
 const manageData = (data) => {
-  const newData = data.split("\n").map(x => x.split("->").map(s => s.trim().split(",").map(i => Number(i))))
+  const newData = data.split('\n').map((x) =>
+    x.split('->').map((s) =>
+      s
+        .trim()
+        .split(',')
+        .map((i) => Number(i))
+    )
+  )
   return newData
 }
 
 const part1 = (d) => {
-  const createGrid = (h = 0, w = 0) => { 
-    let grid = []
-    for (let i = 0; i < h; i++) {
-      grid[i] = []
-      for (let j = 0; j < w; j++) {
-        grid[i][j] = "."
+  const data = manageData(d)
+  const grid = {}
+
+  for (let [[startX, startY], [endX, endY]] of data) {
+    if (!(startX === endX) && !(startY === endY)) continue
+    const [minX, maxX] = [Math.min(startX, endX), Math.max(startX, endX)]
+    const [minY, maxY] = [Math.min(startY, endY), Math.max(startY, endY)]
+
+    for (let y = minY; y < maxY + 1; y++) {
+      for (let x = minX; x < maxX + 1; x++) {
+        grid[`${x},${y}`] ? (grid[`${x},${y}`] += 1) : (grid[`${x},${y}`] = 1)
       }
     }
-    //let horizontal = [...Array(h).keys()].forEach(_ => grid[0])
-    //console.log(horizontal);
-    return grid
   }
-
-  const data = manageData(d)
-  console.log(createGrid(3, 3))
-  for (let [[startX, startY], [endX, endY]] of data) {
-    //console.log(endY);
-  }
+  const overlaps = Object.entries(grid).reduce(
+    (x, [_, i]) => (i > 1 ? x + 1 : x),
+    0
+  )
+  return overlaps
 }
 
 const main = () => {
   fs.readFile('./input/day5.txt', 'utf8', (err, data) => {
     if (err) return console.log(err)
 
-    part1(data)
+    console.log(part1(data))
   })
 }
 
